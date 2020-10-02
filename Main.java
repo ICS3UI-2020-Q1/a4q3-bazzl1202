f2bc0d0d356d27590288e2ae92921fcbb8121678
bazzl1202 <71136617+bazzl1202@users.noreply.github.com>
2020-10-02 12:11:54 -0400

    Calculates sum from 1 to given integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks the user for a positive integer than calculates sum from 1 to that number.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a postive integer
+    System.out.println("Please enter a postive integer:");
+
+    // declare and initialize the variable
+    int posInt = input.nextInt();
+
+    // create a variable to keep track of the number
+    int count = 1;
+    
+    // the accumulator to add the count into
+    int sum = 0;
+
+    // using a loop to count up to the users integer
+    while( count <= posInt ){
+      // add the numbers as we go into sum
+      sum = sum + count;
+      // increase the value of count by 1
+      count = count + 1; 
+    }
+    System.out.println("The sum from 1 - " + posInt + " is " + sum);
+
+
+
+
     
   }
 }
